382ef25e6c86569cf15d61812481888e9952822c
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-19 18:54:17 +0800

    2020年07月19日 18:17的快速提交

--- src/scripts/publish-process.js
+++ src/scripts/publish-process.js
@@ -1,4 +1,5 @@
 const execa = require("execa");
+const git = require("simple-git")();
 const toast = require("@/utils/toast");
 const versionCommit = require("@/scripts/version-commit");
 const selectVersionType = require("@/scripts/select-version-type");
@@ -6,11 +7,12 @@ const appendRemoteInfo = require("@/scripts/append-remote-info");
 
 const hasRemote = require("@/utils/hasRemote");
 const caniUseName = require("@/utils/caniuse-name");
+const getLibraryJson = require("@/utils/get-library-json");
 
-
-const { name } = require("../../package.json");
 
 module.exports = async () => {
+  await git.init();
+  const { name } = await getLibraryJson();
   if (!await caniUseName()) {
     toast.warn(["包名", name, "不符合规范!"].join(""));
     process.exit(0);
--- src/utils/caniuse-name.js
+++ src/utils/caniuse-name.js
@@ -1,10 +1,21 @@
+const toast = require("@/utils/toast");
+const npmName = require("npm-name");
 const validate = require("validate-npm-package-name");
-const { name } = require("../../package.json");
+const getLibraryJson = require("@/utils/get-library-json");
 
 module.exports = async () => {
   try {
+    const { name } = await getLibraryJson();
+    toast.start(["正在查询包名(", name, ")是否可用", "... ..."].join(""));
     const { validForNewPackages, validForOldPackages } = await validate(name);
-    return (validForNewPackages && validForOldPackages);
+    const isValidate = (validForNewPackages && validForOldPackages);
+    const isAvailable = await npmName(name);
+    if (isValidate && isAvailable) {
+      toast.succeed(["包名(", name, ")可用!"].join(""));
+      return true;
+    };
+    toast.fail(["包名(", name, ")不可用!"].join(""));
+    return false;
   } catch (error) {
     throw error;
   };
